47d85326c6c21e8d76d5a6b68af4fd706f90f83f
bambshot <takeshima.nitkc@gmail.com>
2020-06-09 02:44:09 +0900

    [add] add css modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['modern-css-reset'],
+  css: ['modern-css-reset', '~/assets/variables.scss', '~/assets/common.scss'],
   /*
    ** Plugins to load before mounting the App
    */
