f425a7c90ac7f6e85bbef8d70a9fd534c8db89fa
bambshot <takeshima.nitkc@gmail.com>
2020-06-11 01:21:38 +0900

    [add] article list page

---
+++ pages/articles/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>一覧</h1>
+    <ul class="articleList">
+      <li v-for="(article, index) in articles" :key="index" class="article">
+        <nuxt-link :to="`${article.path}`">
+          {{ article.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const articles = await $content('articles')
+      .sortBy('createdAt')
+      .fetch()
+    return {
+      articles
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
