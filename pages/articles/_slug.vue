1e80eec3c0e9bd1907278996c5b4d99577fa5498
bambshot <takeshima.nitkc@gmail.com>
2020-06-11 01:21:18 +0900

    [add] add markdown pages

---
+++ pages/articles/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $content }) {
+    const article = await $content('articles', params.slug).fetch()
+    return {
+      article
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/article';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('index').fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/article';
+</style>
