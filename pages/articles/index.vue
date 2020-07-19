80733726634c674d3ef3ef72a1c92ca33c4a8f82
bambshot <takeshima.nitkc@gmail.com>
2020-07-19 16:58:02 +0900

    [update] adjust article list page

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <h1>一覧</h1>
+    <h1>記事一覧</h1>
     <ul class="articleList">
-      <li v-for="(article, index) in articles" :key="index" class="article">
+      <li v-for="(article, index) in articles" :key="index" class="articleItem">
         <nuxt-link :to="`${article.path}`">
-          {{ article.title }}
+          <time>{{ formatedCreatedAtList[index] }}</time>
+          <h2>{{ article.title }}</h2>
         </nuxt-link>
       </li>
     </ul>
@@ -15,13 +16,43 @@
 export default {
   async asyncData({ $content }) {
     const articles = await $content('articles')
-      .sortBy('createdAt')
+      .only(['path', 'title', 'createdAt'])
+      .sortBy('createdAt', 'desc')
       .fetch()
+    const formatedCreatedAtList = articles.map((article) => {
+      const date = new Date(article.createdAt)
+      const yyyy = date.getFullYear()
+      const mm = ('00' + (date.getMonth() + 1)).slice(-2)
+      const dd = ('00' + date.getDate()).slice(-2)
+      return `${yyyy}/${mm}/${dd}`
+    })
+    console.log(articles)
     return {
-      articles
+      articles,
+      formatedCreatedAtList
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 1.5em;
+}
+
+.articleItem {
+  border-top: 1px solid var(--grey);
+  &:last-child {
+    border-bottom: 1px solid var(--grey);
+  }
+  a {
+    display: block;
+    padding: 40px 0;
+    text-decoration: none;
+    color: var(--black);
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
