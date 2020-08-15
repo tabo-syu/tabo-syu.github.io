0caf1878055999bd5a8f728a6a9cd1f6616e8bec
bambshot <bambshot@users.noreply.github.com>
2020-08-15 14:39:50 +0000

    deploy: 808203d8213e6a3b4f5eabada49626193430ceac

---
+++ _nuxt/static/1597502387/articles/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/articles", {data:[{articles:[{title:"テレワーク開始から4か月経ったので、その間聴いてた曲をまとめてみる",path:"\u002Farticles\u002Ffavorite-songs",createdAt:"2020-08-15T14:39:03.537Z"}],formatedCreatedAtList:["2020\u002F08\u002F15"]}],fetch:[],mutations:void 0});
